82894023d7d7f6361ec7f8efa9c9b3589e0043bb
kagemeka <kagemeka1@gmail.com>
2022-02-09 23:57:43 +0000

    ENV: set poetry plugins

--- src/package_2/__init__.py
+++ src/package_2/__init__.py
@@ -1,3 +1,6 @@
-import pkg_resources
+import importlib.metadata
 
-__version__ = pkg_resources.get_distribution("python-project-template").version
+__version__ = importlib.metadata.version("python-project-template")
+# another method to check the package version
+# it's newer than pkg_resources
+# https://docs.python.org/3/library/importlib.metadata.html#module-importlib.metadata
