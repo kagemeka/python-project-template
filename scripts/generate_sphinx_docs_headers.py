6f337c9c9f7bc43fe8c7329cad4d9ef75dcbd4a1
kagemeka <kagemeka1@gmail.com>
2022-02-13 05:48:11 +0000

    docs: update project name

--- scripts/generate_sphinx_docs_headers.py
+++ scripts/generate_sphinx_docs_headers.py
@@ -5,7 +5,7 @@ import sys
 sys.path.append(".")
 from scripts._docs_util import ROOT, get_sphinx_docs_directory
 
-SPHINX_DOCS_HEADER = "Optext"
+SPHINX_DOCS_HEADER = "Python\ Project\ Template"
 
 
 def _remove_current_docs(docs_path: str) -> None:
